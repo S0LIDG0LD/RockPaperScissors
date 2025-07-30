17297da90ac284baa6abbc85517eff05ad73199c
Fabio “SolidGold” Paduanella <fabio.paduanella@truufles.com>
2025-07-30 11:04:06 +0100

    6. Wrote the logic and play until the first player wins 3 rounds or all the 5 rounds.

--- javascript.js
+++ javascript.js
@@ -5,16 +5,12 @@
 /* 2. Create function getComputeChoice that will randomly return Rock, Paper, Scissor. DONE! */
 
 function getComputerChoice() {
-    switch (Math.floor(Math.random() * 3)) {
-        case 0 : return "Rock";
-        case 1 : return "Paper";
-        case 2 : return "Scissors";
-    }
-};
-
-let computerChoice = getComputerChoice();
-
-console.log(computerChoice);
+        switch (Math.floor(Math.random() * 3)) {
+            case 0 : return "Rock";
+            case 1 : return "Paper";
+            case 2 : return "Scissors";
+        }
+    };
 
 /* 3. Create function getHumanChoice that will return one of the valid inputs from user. */
 
@@ -22,7 +18,7 @@ function getHumanChoice() {
 
     while (1) {
     
-        let humanInput = String(prompt("Please enter Rock, Paper or Scissors: "));
+        let humanInput = String(prompt("Round " + (i + 1) + ": Please choose Rock, Paper or Scissors: "));
 
         switch (humanInput.toLowerCase()) {
             case "rock" : return "Rock";
@@ -31,57 +27,83 @@ function getHumanChoice() {
             default : break;
         }
     }
-}
+};
 
-let humanChoice = getHumanChoice();
+const ROUNDS_OF_PLAY = 5;
 
-console.log(humanChoice);
+let computerChoice;
+let humanChoice;
 
 /* 4. Keep track of Player Score using variables. */
-
 let humanScore = 0;
 let computerScore = 0;
 
 console.log("Human Score: " + humanScore);
 console.log("Computer Score: " + computerScore);
 
-/* 5. Write the logic and play a single round. */
+/* 6. Write the logic and play 5 rounds. */
+for (i = 0; i < ROUNDS_OF_PLAY; i++) {
 
-if (humanChoice !== computerChoice) {
-    if (humanChoice === "Rock") {
-        if (computerChoice === "Paper") {
-            computerScore++;
-        }
-        else if (computerChoice === "Scissors") {
-           humanScore++;
-        }
-    }
-    else if (humanChoice === "Paper") {
-        if (computerChoice === "Rock") {
+    console.log("Round: " + (i + 1))
+
+    computerChoice = getComputerChoice();
+    console.log("CC " + computerChoice);
+
+    humanChoice = getHumanChoice();
+    console.log("HC " + humanChoice);
+
+    /* 5. Write the logic and play a single round. */
+
+    if (humanChoice !== computerChoice) {
+        if (humanChoice === "Rock") {
+            if (computerChoice === "Paper") {
+                computerScore++;
+            }
+            else if (computerChoice === "Scissors") {
             humanScore++;
+            }
         }
-        else if (computerChoice === "Scissors") {
-           computerScore++;
-        }
-    }
-    else if (humanChoice === "Scissors") {
-        if (computerChoice === "Rock") {
+        else if (humanChoice === "Paper") {
+            if (computerChoice === "Rock") {
+                humanScore++;
+            }
+            else if (computerChoice === "Scissors") {
             computerScore++;
+            }
         }
-        else if (computerChoice === "Paper") {
-           humanScore++;
+        else if (humanChoice === "Scissors") {
+            if (computerChoice === "Rock") {
+                computerScore++;
+            }
+            else if (computerChoice === "Paper") {
+            humanScore++;
+            }
         }
+
+        alert(humanScore > computerScore ? (
+        "I picked " + computerChoice + ", YOU WON!") : (
+        "I picked " + computerChoice + ", YOU LOOSE!")
+        );
+
+        alert("Human Score: " + humanScore + ", Computer Score: " + computerScore);
+
+        console.log("Human Score: " + humanScore);
+        console.log("Computer Score: " + computerScore);
+
+        if (humanScore > 2 || computerScore > 2) {
+            break;
+        }
+
+    } else if (humanChoice === computerChoice) {
+        i--
+        alert("AAAARGHH! We both picked " + computerChoice); 
     }
 }
 
-alert(humanScore > computerScore ? (
-    "I picked " + computerChoice + ", YOU WON!") : (
-    "I picked " + computerChoice + ", YOU LOOSE!")
-);
+if (humanScore > computerScore) {
+        alert("GAME OVER, YOU WON!");
+} else {
+        alert("GAME OVER, I WON!");
+};
 
-console.log("Human Score: " + humanScore);
-console.log("Computer Score: " + computerScore);
-
-
-/* 6. Write the logic and play 5 rounds. */
 
